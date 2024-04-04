e2f48f8b3c24ac228ddb9d2589f63eeba503729a
Kirill Mogorovskiy <kirill2001555@gmail.com>
2024-04-04 13:38:02 +0300

    SECOND. - Rewrite UristyAttorneyProfileUrlsScraper class - Change title selector

--- src/main/java/com/mogorovskiy/attorneyparser/parser/uristyua/UristyAttorneyProfileUrlsScraper.java
+++ src/main/java/com/mogorovskiy/attorneyparser/parser/uristyua/UristyAttorneyProfileUrlsScraper.java
@@ -1,44 +1,36 @@
 package com.mogorovskiy.attorneyparser.parser.uristyua;
 
-import com.mogorovskiy.attorneyparser.parser.*;
-import org.jsoup.*;
-
-import java.io.*;
-import java.util.*;
-import java.util.regex.*;
+import com.mogorovskiy.attorneyparser.parser.AttorneyProfileUrlsScraper;
+import org.jsoup.Jsoup;
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UristyAttorneyProfileUrlsScraper implements AttorneyProfileUrlsScraper {
     private static final String LISTING_URL = "https://uristy.ua/lawyers/cernigiv";
     private static final String PROFILE_URLS_SELECTOR = "p[class=lead] a";
+    private static final int MINIMAL_NUMBER_OF_ID_CHARACTERS = 4;
 
     @Override
     public List<String> scrape() throws IOException {
-        List<String> allAttorneysUrls = Jsoup.connect(LISTING_URL).get()
+        return Jsoup.connect(LISTING_URL).get()
                 .select(PROFILE_URLS_SELECTOR).stream()
                 .map(element -> element.absUrl("href"))
+                .filter(attorneyUrl -> {
+                    String id = extractLastDigits(attorneyUrl);
+                    return id != null && id.length() > MINIMAL_NUMBER_OF_ID_CHARACTERS;
+                })
                 .toList();
-        return filterAttorneysWithPhoto(allAttorneysUrls);
     }
 
-    private List<String> filterAttorneysWithPhoto(List<String> allAttorneys) {
-        List<String> attorneysWithPhoto = new ArrayList<>();
-        for (String attorney: allAttorneys) {
-            String id = extractLastDigits(attorney);
-            assert id != null;
-            if (id.length() > 4) {
-                attorneysWithPhoto.add(attorney);
-            }
-        }
-        return attorneysWithPhoto;
-    }
-
-    public static String extractLastDigits(String url) {
+    private String extractLastDigits(String url) {
         Pattern pattern = Pattern.compile("\\d+$");
         Matcher matcher = pattern.matcher(url);
         if (matcher.find()) {
             return matcher.group();
         } else {
-            return null;
+            return "";
         }
     }
 }
